f2e66e8c517f4bb6af513c256b71ae9170fcbb1c
HARSHAVARDHAN GUDEM <harsah.gudem@gmail.com>
2024-08-27 14:21:14 -0600

    added api

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-<DOCTYPE html/>
-<html>
-    
-</html>
+const searchBox = document.querySelector(".searchBox");
+const searchBtn =document.querySelector(".searchBtn");
+const recipeContainer =document.querySelector(".recipe-container");
+
+const fetchRecipes = async (searchInput)=>{
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`);
+    const data = await response.json();
+    console.log(data.meals);
+}
+searchBtn.addEventListener("click",(e)=> {
+    e.preventDefault();
+    const searchInput = searchBox.value.trim();
+    fetchRecipes(searchInput);
+})
