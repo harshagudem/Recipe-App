8f337a550099f2e905191dd522824a3f6083b27f
harshagudem <157737492+harshagudem@users.noreply.github.com>
2024-08-31 23:52:38 -0600

    js changes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const recipeContainer =document.querySelector(".recipe-container");
 const fetchRecipes = async (searchInput)=>{
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`);
     const data = await response.json();
-    console.log(data.meals);
+    console.log(data.meals); 
 }
 searchBtn.addEventListener("click",(e)=> {
     e.preventDefault();
